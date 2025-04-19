b09a6dbbbaff096080554ec91142de81c7abb102
Derek Berger <derekberger734@gmail.com>
2025-04-19 08:52:15 -0400

    Remove security lookup

--- terraform/data.tf
+++ terraform/data.tf
@@ -6,17 +6,3 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_account_alias" "current" {}
-
-###############################
-# TFC Workspace Data Provider #
-###############################
-data "terraform_remote_state" "security_account_remote_state" {
-  backend = "remote"
-
-  config = {
-    organization = "biotornic"
-    workspaces = {
-      name = var.security_account_tfc_workspace_name
-    }
-  }
-}
